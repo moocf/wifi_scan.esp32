814eb9b9e227f62edb6c08914bdfcda56d0e89dd
Subhajit Sahu <wolfram77@gmail.com>
2019-06-24 22:31:48 +0530

    main: remove unused #include

--- main/main.c
+++ main/main.c
@@ -1,9 +1,7 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
-#include "esp_wifi.h"
-#include "esp_log.h"
-#include "esp_event.h"
-#include "nvs_flash.h"
+#include <freertos/FreeRTOS.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
 #include "macros.h"
 
 

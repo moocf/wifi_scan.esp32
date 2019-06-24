8e9443c82b1d13891442e61951892bb6bea64d9f
Subhajit Sahu <wolfram77@gmail.com>
2019-06-24 21:40:46 +0530

    macros: update ERET with help from ESP_ERROR_CHECK

--- main/macros.h
+++ main/macros.h
@@ -1,12 +1,19 @@
 #pragma once
+#include <esp_err.h>
 
-#define ERET(cmd) cmd;
 
-/* #define ERET(cmd) \
-  do { \
-    esp_err_t ret = cmd; \
-    if (ret == ESP_OK) break; \
-    ESP_LOGE(TAG, "%s:%d %s", __FILE__ , __LINE__, esp_err_to_name(ret)); \
-    return ret; \
+#if defined(NDEBUG) || defined(ERET_ABORT)
+#define ERET(x) ESP_ERROR_CHECK(x)
+#elif defined(CONFIG_COMPILER_OPTIMIZATION_ASSERTIONS_SILENT)
+#define ERET(x) do {                          \
+    esp_err_t __err_rc = (x);                 \
+    if (__err_rc != ESP_OK) return __err_rc;  \
   } while(0)
- */
+#else
+#define ERET(x) do {                                                            \
+    esp_err_t __err_rc = (x);                                                   \
+    if (__err_rc != ESP_OK) {                                                   \
+      _esp_error_check_failed(__err_rc, __FILE__, __LINE__, __ASSERT_FUNC, #x); \
+      return __err_rc;                                                          \
+    } while(0)
+#endif
